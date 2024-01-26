4bf401ffb665415df0daec37ea6a78b3e090dadd
Ondrej Metelka <ometelka@redhat.com>
2024-01-26 16:35:52 +0100

    update logging conf

--- ols/utils/logging.py
+++ ols/utils/logging.py
@@ -1,17 +1,37 @@
 """Logging utilities."""
 
-import logging
+import logging.config
 
 from ols.app.models.config import LoggingConfig
 
 
 def configure_logging(logging_config: LoggingConfig) -> None:
     """Configure application logging according to the configuration."""
-    logging.basicConfig(
-        level=logging_config.app_log_level,
-        format="%(asctime)s [%(name)s:%(filename)s:%(lineno)d] %(levelname)s: %(message)s",
-    )
-    # manage logging levels for external libraries
-    logging.getLogger("httpcore").setLevel(logging_config.library_log_level)
-    logging.getLogger("httpx").setLevel(logging_config.library_log_level)
-    logging.getLogger("urllib3").setLevel(logging_config.library_log_level)
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "root": {  # root logger
+            "level": logging_config.library_log_level,
+            "handlers": ["console"],
+        },
+        "loggers": {
+            "ols": {
+                "level": logging_config.app_log_level,
+                "handlers": ["console"],
+                "propagate": False,
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "standard",
+            },
+        },
+        "formatters": {
+            "standard": {
+                "format": "%(asctime)s [%(name)s:%(filename)s:%(lineno)d] %(levelname)s: %(message)s"  # noqa E501
+            },
+        },
+    }
+
+    logging.config.dictConfig(logging_config)
